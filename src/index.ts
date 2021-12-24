52c10029b7261bd3887dac2252c29c6b0bfb6181
zhao-huo-long <lijiuyi1995@outlook.com>
2021-12-24 10:47:05 +0800

    add comment

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,58 @@
 import val from 'assert-tiny'
 
-type F = (...a: any[]) => any
+export type F = (...a: any[]) => any
 
-type EventBus = {
+export type EventBus = {
   [eventName: string]: F[]
 }
 
 export class Emitter {
   protected eventBus: EventBus = {}
+  /**
+   * add listener 
+   * @param eventName String
+   * @param cb Function
+   * @returns this
+   */
   public on = (eventName: string, cb: F) => {
     val(eventName).isTypeOf(String, 'eventName must be a string')
     val(cb).isTypeOf(Function, 'callback must be a funtion')
     const { eventBus } = this
-    if(Array.isArray(eventBus[eventName])){
+    if (Array.isArray(eventBus[eventName])) {
       eventBus[eventName].push(cb)
-      return
+    }else{
+      eventBus[eventName] = [cb]
     }
-    eventBus[eventName] = [cb]
     return this
   }
+  /**
+   * emit event
+   * @param eventName String
+   * @param payload Any
+   */
   public emit = (eventName: string, payload: unknown) => {
     val(eventName).isTypeOf(String, 'eventName must be a string')
     const { eventBus } = this
-    if(Array.isArray(eventBus[eventName])){
-      eventBus[eventName].forEach( cb => cb(payload) )
+    if (Array.isArray(eventBus[eventName])) {
+      eventBus[eventName].forEach(cb => cb(payload))
     }
   }
+  /**
+   * remove event listener
+   * @param eventName String
+   * @param cb Function
+   */
   public off = (eventName: string, cb: F) => {
     val(eventName).isTypeOf(String, 'eventName must be a string')
     val(cb).isTypeOf(Function, 'callback must be a funtion')
     const { eventBus } = this
-    if(Array.isArray(eventBus[eventName])){
+    if (Array.isArray(eventBus[eventName])) {
       eventBus[eventName] = eventBus[eventName].filter(func => cb !== func)
     }
   }
-
+  /**  
+   *  event only emit once  
+   * */
   public once = (eventName: string, cb: F) => {
     val(eventName).isTypeOf(String, 'eventName must be a string')
     val(cb).isTypeOf(Function, 'callback must be a funtion')
@@ -46,6 +64,5 @@ export class Emitter {
   }
 }
 
-const emit = new Emitter()
 
-export default emit
+export default Emitter
